8fc18e41079e914aedcb1ded2866e3a484e2802b
bilel-kedidi <bilel.kedidi@gmail.com>
2019-04-18 17:16:58 +0100

    redmine_issue_step_edit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_step_edit do
+  name 'Redmine Issue Step Edit plugin'
+  author 'Bilel KEDIDI'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
